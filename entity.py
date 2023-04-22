25bfa58dd7d17241bb95cafbf48b1cde96cb7e99
halan10 <halanmykola@gmail.com>
2023-04-22 16:51:06 +0300

    update class record

--- entity.py
+++ entity.py
@@ -10,12 +10,20 @@ class Field:
         self.value = value
     def __str__ (self)->str:
         return self.value
+    
+class Name(Field):
+    pass
+
+class Phone(Field):
+    pass
 
 
 class Record:
-    def __init__(self,name):
+    def __init__(self,name, phone:Phone = None):
         self.name = name
-        self.phones = []
+        self.phones = [] 
+        if phone:
+            self.add_phone(phone)
 
     def add_phone(self, phone):
         self.phones.append(phone)
@@ -23,12 +31,10 @@ class Record:
     def change_phone(self, index, phone):
         self.phones[index]=phone
 
+    def __repr__(self) -> str:
+        return ','.join([p.value for p in self.phones])
+
 class AddressBook(UserDict):
     def add_record(self, record:Record):
         self.data[record.name.value]= record
 
-class Name(Field):
-    pass
-
-class Phone(Field):
-    pass
